b1d592628089bb0b2e499000ecce968a8d67b2c4
AmiraliKamali <111501266+AmiraliKamali@users.noreply.github.com>
2025-04-22 12:54:43 +0330

    AI-audio

---
+++ jarvis_chatgpt.py
@@ -0,0 +1,29 @@
+from RealtimeSTT import AudioToTextRecorder
+import assist_chatgpt
+import time
+import tools_chatgpt
+
+
+if __name__ == '__main__':
+    recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en", post_speech_silence_duration =1, silero_sensitivity = 0.1)
+    hot_words = ["hey"]
+    #skip_hot_word_check = False
+    skip_hot_word_check = True
+    print("Say something...")
+    while True:
+        current_text = recorder.text()
+        print(current_text)
+        if any(hot_word in current_text.lower() for hot_word in hot_words) or skip_hot_word_check:
+                    if current_text:
+                        print("User: " + current_text)
+                        recorder.stop()
+                        current_text = current_text + " " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+                        response = assist_chatgpt.ask_question_memory(current_text)
+                        print(response)
+                        speech = response.split('#')[0]
+                        done = assist_chatgpt.TTS(speech)
+                        skip_hot_word_check = True if "?" in response else False
+                        if len(response.split('#')) > 1:
+                            command = response.split('#')[1]
+                            tools_chatgpt.parse_command(command)
+                        recorder.start()
---
+++ jarvis_ollama.py
@@ -0,0 +1,28 @@
+from RealtimeSTT import AudioToTextRecorder
+import assist_ollama
+import time
+
+
+if __name__ == '__main__':
+    recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en", post_speech_silence_duration =0.1, silero_sensitivity = 0.4)
+    hot_words = ["hey"]
+    #skip_hot_word_check = False
+    skip_hot_word_check = True
+    print("Say something...")
+    while True:
+        current_text = recorder.text()
+        print(current_text)
+        if any(hot_word in current_text.lower() for hot_word in hot_words) or skip_hot_word_check:
+                    if current_text:
+                        print("User: " + current_text)
+                        recorder.stop()
+                        current_text = current_text + " " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+                        response = assist_ollama.ask_question_memory(current_text)
+                        print(response)
+                        speech = response.split('#')[0]
+                        done = assist_ollama.TTS(speech)
+                        skip_hot_word_check = True if "?" in response else False
+                        #if len(response.split('#')) > 1:
+                        #    command = response.split('#')[1]
+                        #    tools_ollama.parse_command(command)
+                        #recorder.start()
